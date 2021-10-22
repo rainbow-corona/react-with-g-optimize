32da50ab3bd09b1fbfb44e5225ecfc98dbeaf31a
O.N.S <73737316+rainbow-corona@users.noreply.github.com>
2021-10-22 09:58:30 +0800

    initialize project

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => <h1>Have a good day!</h1>
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
