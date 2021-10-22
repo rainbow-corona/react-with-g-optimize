068bcffb5933af2f9b0929936ce5364ba7fdcb71
O.N.S <73737316+rainbow-corona@users.noreply.github.com>
2021-10-22 10:53:12 +0800

    add package: react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import styled from 'styled-components'
 
-const Greeting = styled.p`
-  width: 100%;
-  padding: 2.5em 0;
-  text-align: center;
-  font-size: 2.5em;
-  color: blueviolet;
-`
-
-const App = () => <Greeting>Hello!</Greeting>
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
